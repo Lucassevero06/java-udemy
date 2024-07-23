41bc436aabe53177b7fd02b0cd856adb57bad052
LUCAS GABRIEL DA SILVA VAZ SEVERO <lucasgabriel31032006@gmail.com>
2024-07-23 19:03:57 -0400

    casting
    casting  é a conversão explicita dos valores

---
+++ src/TesteProcessamento.java
@@ -0,0 +1,37 @@
+public class TesteProcessamento
+{
+    public static void main(String[] args)
+    {
+
+//        double b, B, h, area;
+//
+//        b = 6.0;
+//        B = 8.0;
+//        h = 5.0;
+//
+//        area = (b + B) / 2.0 * h;
+//        System.out.println(area);
+
+//        //quando se usa uam variavel do tipo inteiro, mesmo quando o resultado dá um numero double, ele arredonda o resultado
+//        int a, b;
+//        double resultado;
+//
+//        a = 5;
+//        b = 2;
+//
+//        //casting (conversão explicita dos valores)
+//        resultado = (double) a / b;
+//        System.out.println(resultado);
+
+        double a;
+        int b;
+
+        a = 5.0;
+
+        //casting (conversão explicita dos valores)
+        b = (int) a;
+
+        System.out.println(b);
+
+    }
+}
