9f424b3fd7ae37eca00b708cc27d8940456b86e1
LUCAS GABRIEL DA SILVA VAZ SEVERO <lucasgabriel31032006@gmail.com>
2024-07-23 17:55:45 -0400

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");Streingç
+        System.out.println("Hello world!");
+
+        String nome = "Lucas";
+        System.out.println("Nome: " + nome);
     }
-}5255hgdfgh
+}
