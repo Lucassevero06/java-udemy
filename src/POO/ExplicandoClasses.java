d6cf873cab408e7781194d7da3ee29dd965a8ebb
LUCAS GABRIEL DA SILVA VAZ SEVERO <lucasgabriel31032006@gmail.com>
2024-07-25 15:55:34 -0400

    melhorando o desafio com a classe Triangle

---
+++ src/POO/ExplicandoClasses.java
@@ -0,0 +1,25 @@
+package POO;
+
+public class ExplicandoClasses {
+    public static void main(String[] args) {
+        /*
+        *
+        * CLASSE:
+        *   -É um tipo estruturado que pode conter membros:
+        *           -Variáveis
+        *           -Métodos
+        *   -A classe também pode prover muitos outros recursos, tais como:
+        *           -Construtores
+        *           -Sobrecargas
+        *           -Encapsulamento
+        *           -Herancas
+        *           -Polimorfismo
+        *   -Exemplos:
+        *           -Entidades: Produto, Cliente, Email, Storage
+        *           -Serviços: ProdutoService, ClienteService, EmailService, StorageService
+        *           -Controladores: ProdutoController, ClienteController, EmailController, StorageController
+        *           -Utilitários: Calculadora, Compactador
+        *           -Outros recursos: views, repositórios, gerenciadores, etc
+        * */
+    }
+}
