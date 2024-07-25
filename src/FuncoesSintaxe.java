b4bf5dff29acf68a7a3e22ba710de111ec2bb966
LUCAS GABRIEL DA SILVA VAZ SEVERO <lucasgabriel31032006@gmail.com>
2024-07-25 13:48:56 -0400

    funções sintaxe

---
+++ src/FuncoesSintaxe.java
@@ -0,0 +1,34 @@
+public class FuncoesSintaxe {
+    public static void main(String[] args) {
+
+        /*
+        *
+        * FUNÇÕES:
+        *   -representam um processamento que possui significado
+        *   -uma função pode retornar um valor
+        *   -uma função pode receber um ou mais valores
+        *   -uma função pode ser chamada quantas vezes quiser
+        *   -uma função pode ser chamada dentro de uma expressão
+        *   -uma função pode ser chamada dentro de um bloco
+        *
+        *   PRINCIPAIS VANTAGENS:
+        *       -modularização
+        *       -delegação
+        *       -reaproveitamento
+        *   DADOS DE ENTRADA E SAIDA:
+        *       -funções podem receber dados de entrada (parametros ou argumentos
+        *       -funções podem ou não retornar uma saida
+        *
+        *   EM ORIENTAÇÃO A OBJETOS, FUNÇÕES EM CLASSES RECEBEM O NOME DE "MÉTODOS"
+        *
+        * */
+
+        double y = 25.0;
+        double x = Math.sqrt(y); // essa função retorna um valor
+
+        System.out.println(x); // esssa função não retorna valor, apenas exibe a saida
+
+
+
+    }
+}
