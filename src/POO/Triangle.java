d0d709c40170e8a7a5a1adf538702fc6f3a4e724
LUCAS GABRIEL DA SILVA VAZ SEVERO <lucasgabriel31032006@gmail.com>
2024-07-26 08:04:22 -0400

    Avançando em POO com Java

--- src/POO/Triangle.java
+++ src/POO/Triangle.java
@@ -7,4 +7,9 @@ public class Triangle {
     public double b;
     public double c;
 
+    // metodos
+    public double area() {
+        double p = (a + b + c) / 2.0;
+        return Math.sqrt(p * (p - a) * (p - b) * (p - c));
+    }
 }
