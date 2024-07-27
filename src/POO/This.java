5a2cec768410c8bcd2d9c40e0ab0232cb08a0b32
LUCAS GABRIEL DA SILVA VAZ SEVERO <lucasgabriel31032006@gmail.com>
2024-07-27 13:24:05 -0400

    Create This.java

---
+++ src/POO/This.java
@@ -0,0 +1,17 @@
+package POO;
+
+public class This {
+    public static void main(String[] args) {
+
+        /*
+        * THIS:
+        *
+        *   - é uma referencia para o proprio objeto
+        *
+        *   - usos comuns:
+        *       - diferenciar atributos de variaveis locais
+        *       - passar o proprio objeto como argumento na chamada de um metodo ou contrutor
+        * */
+
+    }
+}
