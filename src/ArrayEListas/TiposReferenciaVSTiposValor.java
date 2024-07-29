36b692e3da90fabfd650f210aa725b067aad016d
LUCAS GABRIEL DA SILVA VAZ SEVERO <lucasgabriel31032006@gmail.com>
2024-07-29 15:00:08 -0400

    Create TiposReferenciaVSTiposValor.java

---
+++ src/ArrayEListas/TiposReferenciaVSTiposValor.java
@@ -0,0 +1,13 @@
+package ArrayEListas;
+
+public class TiposReferenciaVSTiposValor {
+    public static void main(String[] args) {
+
+        /*
+        * CLASSES SÃO TIPOS DE REFERENCIAS:
+        *
+        * VARIAVEIS CUJO TIPO SÃO CLASSES NÃO DEVEM SER ENTENDIDAS COMO CAIXAS, MAS SIM "TENTÁCULOS" (PONTEIROS) PARA CAIXAS
+        * */
+
+    }
+}
