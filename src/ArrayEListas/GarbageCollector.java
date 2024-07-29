10c69ea17de6f67965e7a24cdc87353f7b76ce7b
LUCAS GABRIEL DA SILVA VAZ SEVERO <lucasgabriel31032006@gmail.com>
2024-07-29 18:22:31 -0400

    Create GarbageCollector.java

---
+++ src/ArrayEListas/GarbageCollector.java
@@ -0,0 +1,20 @@
+package ArrayEListas;
+
+public class GarbageCollector {
+    public static void main(String[] args) {
+
+        /*
+        * GARBAGE COLLECTOR:
+        *   -é um processo que automatiza o gerenciamento de memória de um programa em execução
+        *
+        *   -o garbage collector monitora os objetos alocados dinamicamente pelo programa (no heap), desalocando aqueles que não estão mais sendo utilizados.
+        *
+        * RESUMO:
+        *
+        *   -Objetos alocados dinamicamento, quando não possuem maiis refere ncia para eles, serão desalocados pelo garbage collector
+        *
+        *   -Variavies locais são desalocadas imediatamente assim que seu escopo local sai da execução
+        * */
+
+    }
+}
