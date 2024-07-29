71e4574dbb40ed5875b09c00c5a9ddb09977f5b3
LUCAS GABRIEL DA SILVA VAZ SEVERO <lucasgabriel31032006@gmail.com>
2024-07-29 18:14:08 -0400

    Update TiposReferenciaVSTiposValor.java

--- src/ArrayEListas/TiposReferenciaVSTiposValor.java
+++ src/ArrayEListas/TiposReferenciaVSTiposValor.java
@@ -6,7 +6,27 @@ public class TiposReferenciaVSTiposValor {
         /*
         * CLASSES SÃO TIPOS DE REFERENCIAS:
         *
-        * VARIAVEIS CUJO TIPO SÃO CLASSES NÃO DEVEM SER ENTENDIDAS COMO CAIXAS, MAS SIM "TENTÁCULOS" (PONTEIROS) PARA CAIXAS
+        *   VARIAVEIS CUJO TIPO SÃO CLASSES NÃO DEVEM SER ENTENDIDAS COMO CAIXAS, MAS SIM "TENTÁCULOS" (PONTEIROS) PARA CAIXAS
+        *
+        * TIPOS PRIMITIVOS SÃO TIPOS DE VALOR:
+        *
+        *   EM JAVA, TIPOS PRIMITIVOS SÃO TIPOS VALOR. TIPOS VALOR SÃO CAIXAS E NÃO PONTEIROS.
+        *
+        * TIPOS PRIMITIVOS E INICIALIZAÇÃO:
+        *
+        *   int p; <- é necessário informar um valor inicial
+        *
+        *   System.out.println(p); <- retorna um erro pois p não foi inicializado
+        *
+        * VALORES PADROES:
+        *
+        *   QUANDO ALOCAMOS (new) QUALQUER TIPO ESTRUTURADO (CLASSES OU ARRAYS), SÃO ATRIBUIDOS VALORES PADRÃO AOS SEUS ELEMENTOS
+        *
+        *       NUMEROS: 0
+        *       BOOLEAN: false
+        *       CHAR: CARACTERE CÓDIGO 0
+        *       OBJETOS: NULL
+        *
         * */
 
     }
