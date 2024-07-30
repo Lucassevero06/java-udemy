8984869e251f65ccd4f1664a3d14fc6fce78e048
LUCAS GABRIEL DA SILVA VAZ SEVERO <lucasgabriel31032006@gmail.com>
2024-07-30 08:31:15 -0400

    Create BoxingEUnboxingEWrapperClasses.java

---
+++ src/ArrayEListas/BoxingEUnboxingEWrapperClasses.java
@@ -0,0 +1,15 @@
+package ArrayEListas;
+
+public class BoxingEUnboxingEWrapperClasses {
+    public static void main(String[] args) {
+        int x = 20;
+
+        Integer obj = x;
+
+        System.out.println(obj);
+
+        int y = obj * 2;
+
+        System.out.println(y);
+    }
+}
