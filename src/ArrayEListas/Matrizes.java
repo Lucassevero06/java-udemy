da869f4faceeb37e5d60f9ea69e77f5f07e49086
LUCAS GABRIEL DA SILVA VAZ SEVERO <lucasgabriel31032006@gmail.com>
2024-07-30 11:40:18 -0400

    Create Matrizes.java

---
+++ src/ArrayEListas/Matrizes.java
@@ -0,0 +1,23 @@
+package ArrayEListas;
+
+public class Matrizes {
+    public static void main(String[] args) {
+        /*
+        * MATRIZES:
+        *   -em programação, "matriz" é o nome dado a arranjos bidimensionais
+        *       * Atenção: "Vetor de vetores"
+        *
+        *   -arranjo (array) é uma estrutura de dados:
+        *       *homogenea (dados do mesmo tipo)
+        *       *ordenada (elementos acessados por meio de posições)
+        *       *alocada de uma vez só, em um bloco contiguo de memória
+        *
+        *   -vantagens:
+        *       *acesso imediato aos elementos pela sua posição
+        *
+        *   -desvantagens:
+        *       *tamanho fixo
+        *       *dificuldade para se realizar inserções e deleções
+        * */
+    }
+}
