31002ff4a3484806ddd9c0e89de39e9ae728e8ac
LUCAS GABRIEL DA SILVA VAZ SEVERO <lucasgabriel31032006@gmail.com>
2024-08-01 15:03:11 -0400

    continuando o desafio

--- src/EnumeracoesEComposicao/Desafios/Ex01/entities/Worker.java
+++ src/EnumeracoesEComposicao/Desafios/Ex01/entities/Worker.java
@@ -2,24 +2,34 @@ package EnumeracoesEComposicao.Desafios.Ex01.entities;
 
 import EnumeracoesEComposicao.Desafios.Ex01.enums.WorkerLevel;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Worker {
 
     private String name;
     private WorkerLevel level;
     private Double baseSalary;
 
-    public Worker(String name, WorkerLevel level, Double baseSalary) {
+    private Department department;
+    private List<HourContract> contracts = new ArrayList<>();
+
+    public Worker() {
+    }
+
+    public Worker(String name, WorkerLevel level, Double baseSalary, Department department) {
         this.name = name;
         this.level = level;
         this.baseSalary = baseSalary;
+        this.department = department;
     }
 
     public void addContract (HourContract contract) {
-
+        contracts.add(contract);
     }
 
     public void removeContract (HourContract contract) {
-
+        contracts.remove(contract);
     }
 
     public Double income (int year, int month) {
@@ -48,5 +58,13 @@ public class Worker {
 
     public void setBaseSalary(Double baseSalary) {
         this.baseSalary = baseSalary;
+    }
+
+    public Department getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(Department department) {
+        this.department = department;
     }
 }
