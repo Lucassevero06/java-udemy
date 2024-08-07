f954828c35583a0d6c8b68bc7417ec70957d3fd1
LUCAS GABRIEL DA SILVA VAZ SEVERO <lucasgabriel31032006@gmail.com>
2024-08-07 13:19:24 -0400

    estudando java

---
+++ src/EnumeracoesEComposicao/Desafios/Ex02/Main.java
@@ -0,0 +1,9 @@
+package EnumeracoesEComposicao.Desafios.Ex02;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+
+    }
+}
---
+++ src/EnumeracoesEComposicao/Desafios/Ex02/entities/Post.java
@@ -0,0 +1,9 @@
+package EnumeracoesEComposicao.Desafios.Ex02.entities;
+
+import java.util.Date;
+
+public class Post {
+
+    private Date moment;
+
+}
